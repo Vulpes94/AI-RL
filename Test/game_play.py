a93004ad67fb428954da7e626e56e24d77c89bae
Vulpes94 <kjunfi94@gmail.com>
2023-03-14 18:54:57 +0900

    fix: wallcount가 env로 이동함에 따라 코드 수정 및 몬테카를로 플레이어 추가

--- Test/game_play.py
+++ Test/game_play.py
@@ -13,19 +13,24 @@
 #     name: python3
 # ---
 
+# +
 from keras.models import load_model
 from Quoridor import Environment,DQN_player
 
+import numpy as np
+import copy
+
+
+# -
 
 class Human_player():
     def __init__(self):
         self.name = "Human_player"
-        self.wallCount = 10
     
-    def policy(self,env, player):
+    def select_action(self,env, player):
         while True:
             # 가능한 행동을 조사한 후 표시
-            available_action = env.get_action(player,self.wallCount)
+            available_action = env.get_action(player)
             print("possible actions = {}".format(available_action))
             
             # 키보드로 가능한 행동을 입력 받음
@@ -41,10 +46,61 @@ class Human_player():
         return
 
 
+class Monte_Carlo_player():
+    def __init__(self):
+        self.name = "MC_player"
+        self.num_playout = 1000
+    
+    def select_action(self,env, player):
+        # 가능한 행동을 조사한 후 표시
+        available_action = env.get_action(player)
+        V = np.zeros(len(available_action))
+        
+        for i in range(len(available_action)):
+            
+            # 플레이아웃을 반복
+            for j in range(self.num_playout):
+                # 현재 상태를 복사해서 플레이아웃에 사용
+                temp_env = copy.deepcopy(env)
+                
+                # p1이 이기면 1, p2가 이기면 -1
+                self.playout(temp_env,available_action[i],player)
+                if player == temp_env.winner:
+                    V[i] += 1
+        
+        return available_action[np.argmax(V)]
+    
+    # 플레이아웃 재귀 함수
+    # 게임이 종료 상태가 될 때까지 행동을 임의로 선택하는 것을 반복
+    def playout(self,temp_env,action,player):
+        temp_env.move(player,action)
+        temp_env.end_check(player)
+        
+        # 게임 종료 체크
+        if temp_env.done == True:
+            return
+        else:
+            # 플레이어 교체
+            if(player ==1):
+                player = 2
+            else:
+                player = 1
+                
+            # 가능한 행동 조사
+            available_action = temp_env.get_action(player)
+            
+            # 무작위로 행동을 선택
+            action = np.random.randint(len(available_action))
+            self.playout(temp_env,available_action[action],player)
+              
+
+
 # +
-p2 = DQN_player()
-p2.epsilon = 0
-p2.load_weights('Q-p1-15.h5')
+# p2 = DQN_player()
+# p2.epsilon = 0
+# p2.load_weights('Q-p1-5.h5')
+
+p2 = Monte_Carlo_player()
 
 p1 = Human_player()
 
@@ -55,19 +111,15 @@ while True:
     env.board[0][8] = 2
     env.board[16][8] = 1
     
-    # 벽 사용 가능 수 다시 복원
-    p2.wallCount = 10
-    p1.wallCount = 10
-    
     for i in range(10000):
         if(i%2==0):
             player = 2
-            position = p2.policy(env, player)
-            env.move(player ,position, p2)
+            action = p2.select_action(env, player)
+            env.move(player ,action)
         else:
             player = 1
-            position = p1.policy(env, player)
-            env.move(player,position,p1)
+            action = p1.select_action(env, player)
+            env.move(player,action)
         
             
         env.end_check(player)
@@ -87,4 +139,4 @@ while True:
     answer = input("More Game? (y/n)")
 
     if answer == 'n':
-        break           
+        break
